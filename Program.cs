ae8c97ece5cf90ebf8d6329d7b676be136f30343
Admiral H. Curtiss <pikachu025@gmail.com>
2013-01-26 10:35:56 +0000

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HyoutaTools {
+	class Program {
+		static void Main( string[] args ) {
+		}
+	}
+}
