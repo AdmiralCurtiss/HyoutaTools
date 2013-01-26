3fb92dede99e431367dbd910e0add4c396d2ea39
Admiral H. Curtiss <pikachu025@gmail.com>
2013-01-26 18:51:55 +0000

    Importing: DanganRonpa.PakText, GraceNote.DanganRonpa.PakTextImport

---
+++ DanganRonpa/PakText/PakText.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+
+namespace HyoutaTools.DanganRonpa.PakText {
+	struct PakTextEntry {
+		public int Offset;
+		public int OffsetLocation;
+		public String Text;
+
+		public override string ToString() {
+			return Text;
+		}
+	}
+
+	class PakText {
+		public List<PakTextEntry> TextList;
+		public int Offset = 0;
+
+		public PakText() { }
+
+		public PakText( String filename ) {
+			if ( !LoadFile( System.IO.File.ReadAllBytes( filename ) ) ) {
+				throw new Exception( "DRMenuFile: Load Failed!" );
+			}
+		}
+
+		public PakText( byte[] Bytes ) {
+			if ( !LoadFile( Bytes ) ) {
+				throw new Exception( "DRMenuFile: Load Failed!" );
+			}
+		}
+
+		public byte[] CreateFile() {
+			List<Byte> Bytes = new List<byte>();
+
+			int TextAmount = TextList.Count;
+
+			Bytes.AddRange( BitConverter.GetBytes( TextAmount ) );
+
+			int[] startpoints = new int[TextAmount];
+			int total = 8 + TextAmount * 4;
+			for ( int i = 0; i < TextAmount; ++i ) {
+				if ( ( i * 4 + 4 ) != TextList[i].OffsetLocation ) {
+					Console.WriteLine( "Sanity Check failed at Entry " + i.ToString() + "!" );
+					return null;
+				}
+
+				String Text = TextList[i].Text;
+				if ( !Text.EndsWith( "\0" ) ) {
+					Text = Text + '\0';
+				}
+
+				Byte[] bytetext = Encoding.Unicode.GetBytes( Text );
+				startpoints[i] = total;
+				total += bytetext.Length;
+			}
+
+			foreach ( int s in startpoints ) {
+				Bytes.AddRange( BitConverter.GetBytes( s ) );
+			}
+			Bytes.AddRange( BitConverter.GetBytes( total ) );
+
+			for ( int i = 0; i < TextAmount; ++i ) {
+				String Text = TextList[i].Text;
+				if ( !Text.EndsWith( "\0" ) ) {
+					Text = Text + '\0';
+				}
+
+				Byte[] bytetext = Encoding.Unicode.GetBytes( Text );
+				Bytes.AddRange( bytetext );
+			}
+
+			return Bytes.ToArray();
+		}
+
+		public void GetSQL( String ConnectionString ) {
+			SQLiteConnection Connection = new SQLiteConnection( ConnectionString );
+			Connection.Open();
+
+			TextList = new List<PakTextEntry>();
+
+			using ( SQLiteTransaction Transaction = Connection.BeginTransaction() )
+			using ( SQLiteCommand Command = new SQLiteCommand( Connection ) ) {
+				Command.CommandText = "SELECT english, PointerRef FROM Text ORDER BY PointerRef";
+				SQLiteDataReader r = Command.ExecuteReader();
+				while ( r.Read() ) {
+					String SQLText;
+
+					try {
+						SQLText = r.GetString( 0 ).Replace( "''", "'" );
+					} catch ( System.InvalidCastException ) {
+						SQLText = "";
+					}
+
+					int PointerRef = r.GetInt32( 1 );
+
+
+					PakTextEntry d = new PakTextEntry();
+					d.OffsetLocation = PointerRef;
+					d.Text = SQLText;
+					d.Offset = -1;
+					TextList.Add( d );
+				}
+
+				Transaction.Rollback();
+			}
+			return;
+		}
+
+
+		private bool LoadFile( byte[] Bytes ) {
+			int TextAmount = BitConverter.ToInt32( Bytes, 0 );
+
+			if ( TextAmount == 0 ) {
+				Console.WriteLine( "No text found!" );
+				return false;
+			}
+
+			TextList = new List<PakTextEntry>( TextAmount );
+
+			// sanity check: After EOF no further bytes
+			int ProjectedEnd = BitConverter.ToInt32( Bytes, ( TextAmount + 1 ) * 4 );
+			//ProjectedEnd = Util.AlignToByteBoundary(ProjectedEnd, 4);
+			for ( int i = ProjectedEnd; i < Bytes.Length; ++i ) {
+				if ( Bytes[i] != 0x00 ) {
+					Console.WriteLine( "Found bytes after calculated EOF!" );
+					return false;
+				}
+			}
+
+			for ( int i = 1; i <= TextAmount; i++ ) {
+				PakTextEntry e = new PakTextEntry();
+
+				e.OffsetLocation = i * 4;
+				e.Offset = BitConverter.ToInt32( Bytes, e.OffsetLocation );
+				int NextOffset = BitConverter.ToInt32( Bytes, e.OffsetLocation + 4 );
+
+				e.Text = Encoding.Unicode.GetString( Bytes, e.Offset, NextOffset - e.Offset );
+				TextList.Add( e );
+			}
+
+
+			return true;
+		}
+	}
+}
---
+++ GraceNote/DanganRonpa/PakTextImport/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+using HyoutaTools.DanganRonpa.PakText;
+
+namespace HyoutaTools.GraceNote.DanganRonpa.PakTextImport {
+	class ScenarioString {
+		public int Pointer;
+		public String Jpn;
+		public String Eng;
+
+		public ScenarioString( int Pointer, String Jpn, String Eng ) {
+			this.Pointer = Pointer;
+			this.Jpn = Jpn;
+			this.Eng = Eng;
+		}
+	}
+
+	class Program {
+		static void Execute( string[] args ) {
+			if ( args.Length != 4 ) {
+				Console.WriteLine( "Usage: DanganRonpaMenu_GraceNote menu.pak NewDBFile TemplateDBFile GracesJapanese" );
+				return;
+			}
+
+			// templateDB must contain:
+			/***
+			 * CREATE TABLE Text(ID int primary key, StringID int, english text, comment text, updated tinyint, status tinyint, PointerRef integer, IdentifyString text, IdentifyPointerRef integer);
+			 ***/
+
+			//*
+			String Filename = args[0];
+			String NewDB = args[1];
+			String TemplateDB = args[2];
+			String GracesDB = args[3];
+			//*/
+
+			/*
+			String Filename = @"c:\Users\Georg\Downloads\Xillia Script files\69753.SDBJPN";
+			String NewDB = @"c:\Users\Georg\Downloads\Xillia Script files\X69753";
+			String TemplateDB = @"c:\Users\Georg\Downloads\Xillia Script files\XTemplate";
+			String GracesDB = @"c:\Users\Georg\Downloads\Xillia Script files\GracesJapanese";
+			//*/
+
+			PakText DRMF;
+			try {
+				DRMF = new PakText( Filename );
+			} catch ( Exception ex ) {
+				Console.WriteLine( ex.Message );
+				Console.WriteLine( "Failed loading menu file!" );
+				return;
+			}
+			Console.WriteLine( "Found " + DRMF.TextList.Count + " entries, importing..." );
+			System.IO.File.Copy( TemplateDB, NewDB, true );
+			InsertSQL( DRMF, "Data Source=" + NewDB, "Data Source=" + GracesDB );
+			Console.WriteLine( "Successfully imported entries!" );
+
+			//byte[] newfile = DRMF.CreateFile();
+			//System.IO.File.WriteAllBytes(Filename + ".new", newfile);
+
+			//System.IO.File.WriteAllBytes(@"C:\TROPHY\newTrophyConf.trp", Encoding.UTF8.GetBytes(TROPSFM.ExportTropSFM(true)));
+			//System.IO.File.WriteAllBytes(@"C:\TROPHY\newTrophy.trp", Encoding.UTF8.GetBytes(TROPSFM.ExportTropSFM(false)));
+
+			return;
+		}
+
+
+
+
+		public static bool InsertSQL( PakText DRTF, String ConnectionString, String ConnectionStringGracesJapanese ) {
+			SQLiteConnection Connection = new SQLiteConnection( ConnectionString );
+			SQLiteConnection ConnectionGracesJapanese = new SQLiteConnection( ConnectionStringGracesJapanese );
+			Connection.Open();
+			ConnectionGracesJapanese.Open();
+
+			using ( SQLiteTransaction Transaction = Connection.BeginTransaction() )
+			using ( SQLiteTransaction TransactionGracesJapanese = ConnectionGracesJapanese.BeginTransaction() )
+			using ( SQLiteCommand Command = new SQLiteCommand( Connection ) )
+			using ( SQLiteCommand CommandGracesJapanese = new SQLiteCommand( ConnectionGracesJapanese ) )
+			using ( SQLiteCommand CommandJapaneseID = new SQLiteCommand( ConnectionGracesJapanese ) )
+			using ( SQLiteCommand CommandSearchJapanese = new SQLiteCommand( ConnectionGracesJapanese ) ) {
+				SQLiteParameter JapaneseIDParam = new SQLiteParameter();
+				SQLiteParameter JapaneseParam = new SQLiteParameter();
+				SQLiteParameter EnglishIDParam = new SQLiteParameter();
+				SQLiteParameter StringIDParam = new SQLiteParameter();
+				SQLiteParameter EnglishParam = new SQLiteParameter();
+				SQLiteParameter PointerRefParam = new SQLiteParameter();
+				SQLiteParameter JapaneseSearchParam = new SQLiteParameter();
+				SQLiteParameter EnglishStatusParam = new SQLiteParameter();
+
+
+				CommandGracesJapanese.CommandText = "INSERT INTO Japanese (ID, string, debug) VALUES (?, ?, 0)";
+				CommandGracesJapanese.Parameters.Add( JapaneseIDParam );
+				CommandGracesJapanese.Parameters.Add( JapaneseParam );
+
+				Command.CommandText = "INSERT INTO Text (ID, StringID, english, comment, updated, status, PointerRef) VALUES (?, ?, ?, \"\", 0, ?, ?)";
+				Command.Parameters.Add( EnglishIDParam );
+				Command.Parameters.Add( StringIDParam );
+				Command.Parameters.Add( EnglishParam );  // Line.SENG
+				Command.Parameters.Add( EnglishStatusParam );
+				Command.Parameters.Add( PointerRefParam ); // Line.Location
+
+				CommandJapaneseID.CommandText = "SELECT MAX(ID)+1 FROM Japanese";
+
+				CommandSearchJapanese.CommandText = "SELECT ID FROM Japanese WHERE string = ? AND debug = 0";
+				CommandSearchJapanese.Parameters.Add( JapaneseSearchParam );
+
+				int JPID;
+				object JPMaxIDObject = CommandJapaneseID.ExecuteScalar();
+				int JPMaxID;
+				try {
+					JPMaxID = Int32.Parse( JPMaxIDObject.ToString() ); // wtf why doesn't this work directly?
+				} catch ( System.FormatException ) {
+					// there's no ID in the database, just start with 0
+					JPMaxID = 0;
+				}
+				int ENID = 1;
+
+				foreach ( PakTextEntry entry in DRTF.TextList ) {
+					// fetch GracesJapanese ID or generate new & insert new text
+					JapaneseSearchParam.Value = entry.Text;
+					object JPIDobj = CommandSearchJapanese.ExecuteScalar();
+					if ( JPIDobj != null ) {
+						JPID = (int)JPIDobj;
+					} else {
+						JPID = JPMaxID++;
+						JapaneseIDParam.Value = JPID;
+						JapaneseParam.Value = entry.Text;
+						CommandGracesJapanese.ExecuteNonQuery();
+					}
+
+					// insert text into English table
+					EnglishIDParam.Value = ENID;
+					StringIDParam.Value = JPID;
+					EnglishParam.Value = entry.Text;
+					EnglishStatusParam.Value = 0;
+					PointerRefParam.Value = entry.OffsetLocation;
+					Command.ExecuteNonQuery();
+
+					ENID++;
+				}
+				Transaction.Commit();
+				TransactionGracesJapanese.Commit();
+			}
+			ConnectionGracesJapanese.Close();
+			Connection.Close();
+
+			return true;
+		}
+	}
+}
