3ecf96fb74018c1bd8f322f3930e5c74c88502cc
Admiral H. Curtiss <pikachu025@gmail.com>
2013-05-05 14:08:41 +0000

    Tales.DestinyRemake.MglkExtract, except all it extracts is the scenario file since that's all we need.

--- Tales/DestinyRemake/TblBin/TBL.cs
+++ Tales/DestinyRemake/TblBin/TBL.cs
@@ -5,10 +5,10 @@ using System.Text;
 using System.IO;
 
 namespace HyoutaTools.Tales.DestinyRemake.TblBin {
-	// THIS DOESN'T APPEAR TO BE CORRECT
 	class TblBinFileReference {
 		public uint Start;
-		public uint Unknown;
+		// File might be compressed, in that case this stores not the filesize of the current, compressed file, but rather how big it is after decompression
+		public uint FilesizeOfDecompressedFile;
 
 		public uint End;
 	}
@@ -16,6 +16,7 @@ namespace HyoutaTools.Tales.DestinyRemake.TblBin {
 	class TBL {
 
 		public List<TblBinFileReference> Refs;
+		public const int FileAlignment = 64;
 
 		public void Load( byte[] Tbl ) {
 			Refs = new List<TblBinFileReference>();
@@ -23,8 +24,12 @@ namespace HyoutaTools.Tales.DestinyRemake.TblBin {
 			for ( int i = 0; i < Tbl.Length - 8; i += 8 ) {
 				TblBinFileReference r = new TblBinFileReference();
 				r.Start = BitConverter.ToUInt32( Tbl, i );
-				r.Unknown = BitConverter.ToUInt32( Tbl, i + 4 );
+				// this does some weird ass stuff with the file locations, last 6 bits of the location are used for the padding until the next file after the current one or something...?
+				uint PaddingAfterFile = ( r.Start % FileAlignment ); 
+				r.Start -= PaddingAfterFile;
+				r.FilesizeOfDecompressedFile = BitConverter.ToUInt32( Tbl, i + 4 );
 				r.End = BitConverter.ToUInt32( Tbl, i + 8 );
+				r.End = r.End - ( r.End % FileAlignment ) - PaddingAfterFile;
 				Refs.Add( r );
 			}
 		}
