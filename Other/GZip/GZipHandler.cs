5024355a6c0513be30f44b1f479f9732ed352a51
Admiral H. Curtiss <pikachu025@gmail.com>
2013-08-06 02:16:00 +0200

    Move GZip handling out of AutoExtract into own class

---
+++ Other/GZip/GZipHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.IO.Compression;
+
+namespace HyoutaTools.Other.GZip {
+	class GZipHandler {
+		public static void Extract( FileStream fs, string outpath ) {
+			fs.Position = 0;
+			var outfile = new FileStream( outpath, System.IO.FileMode.Create );
+			using ( var decompressed = new GZipStream( fs, CompressionMode.Decompress ) ) {
+				byte[] buffer = new byte[4096];
+				int numRead;
+				while ( ( numRead = decompressed.Read( buffer, 0, buffer.Length ) ) != 0 ) {
+					outfile.Write( buffer, 0, numRead );
+				}
+			}
+			outfile.Close();
+		}
+	}
+}
