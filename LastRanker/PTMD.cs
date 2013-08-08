7ddd8305a90b318b592ecdb6d8fb51414b6f049b
Admiral H. Curtiss <pikachu025@gmail.com>
2013-08-08 05:22:51 +0200

    LastRanker/PTMD: Guessing at the actual header format, don't think it's 100% right but works decently enough.

--- LastRanker/PTMD.cs
+++ LastRanker/PTMD.cs
@@ -31,8 +31,10 @@ namespace HyoutaTools.LastRanker {
 
 		public uint Magic;
 		public uint Uint0x04;
-		public uint Uint0x08;
+		public byte BitPerPixelByte;
+		public byte ImageTypeByte;
 		public uint PalettePointer;
+
 		public byte[] ImageBytes;
 		public byte[] PaletteBytes;
 
@@ -45,7 +47,8 @@ namespace HyoutaTools.LastRanker {
 			Magic = BitConverter.ToUInt32( File, 0x00 );
 			Uint0x04 = BitConverter.ToUInt32( File, 0x04 );
 			ImageWidth = 1u << (int)( Uint0x04 >> 28 );
-			Uint0x08 = BitConverter.ToUInt32( File, 0x08 );
+			BitPerPixelByte = File[0x08];
+			ImageTypeByte = File[0x09];
 			PalettePointer = BitConverter.ToUInt32( File, 0x0C );
 
 			ImageBytes = new byte[PalettePointer - 0x10];
@@ -53,14 +56,33 @@ namespace HyoutaTools.LastRanker {
 			PaletteBytes = new byte[File.Length - PalettePointer];
 			Util.CopyByteArrayPart( File, (int)PalettePointer, PaletteBytes, 0, PaletteBytes.Length );
 
-			// i'm sure there's a better way to identify this stuff...
-			switch ( PaletteBytes.Length ) {
-				case 0x400: BitPerPixel = 8; Greyscale = false; Paletted = true; break;
-				case 0x40: BitPerPixel = 4; Greyscale = false; Paletted = true; break;
-				case 0x20: BitPerPixel = 4; Greyscale = true; Paletted = true; break;
-				case 0x0: BitPerPixel = 16; Greyscale = false; Paletted = false; break;
-				default: throw new Exception( "PTMD: Unknown bit per pixel: " + PaletteBytes.Length + " Bytes in Palette" );
+			//*
+			Console.Write( "PTMD: Header: " );
+			for ( int i = 4; i < 12; ++i ) {
+				Console.Write( File[i].ToString( "X2" ) + " " );
 			}
+			Console.Write( " / size: ~" + PalettePointer.ToString( "D6" ) );
+			//*/
+
+			switch ( BitPerPixelByte ) {
+				case 0x04: BitPerPixel = 4; break;
+				case 0x05: BitPerPixel = 8; break;
+				case 0x01: BitPerPixel = 16; break;
+				case 0x03: BitPerPixel = 32; break;
+				case 0x00: BitPerPixel = 16; break; // seems like a nonsense file, probably early version or something?
+				default: throw new Exception( "PTMD: Unknown bit per pixel byte: 0x" + BitPerPixelByte.ToString( "X2" ) );
+			}
+			switch ( ImageTypeByte ) {
+				case 0x20: Greyscale = true; Paletted = true; break;
+				case 0x30: Greyscale = false; Paletted = true; break;
+				case 0x00: Greyscale = false; Paletted = false; break;
+				case 0x10: Greyscale = true; Paletted = true; break; // ???
+				default: throw new Exception( "PTMD: Unknown Image Type Byte: 0x" + ImageTypeByte.ToString( "X2" ) );
+			}
+
+			//*
+			Console.WriteLine( " w: " + ImageWidth.ToString( "D4" ) + " / bpp: " + BitPerPixel.ToString( "D2" ) + " / grey: " + Greyscale + " / pal: " + Paletted );
+			//*/
 
 			return true;
 		}
@@ -90,13 +112,19 @@ namespace HyoutaTools.LastRanker {
 					w = 8;
 					h = 8;
 					break;
+				case 32:
+					TotalPixels = ImageBytes.Length / 4;
+					ImageHeight = TotalPixels / (int)ImageWidth;
+					w = 4;
+					h = 4;
+					break;
 			}
 			int wh = w * h;
 
 			Bitmap bmp = new Bitmap( (int)ImageWidth, ImageHeight );
 			int BlocksPerLine = bmp.Width / w;
 
-			// image in blocks of 16x8 px
+			// image in blocks of w x h px
 			for ( int i = 0; i < TotalPixels; ++i ) {
 				byte r, g, b, a;
 				int idx = 0;
@@ -113,6 +141,7 @@ namespace HyoutaTools.LastRanker {
 								idx = ImageBytes[i / 2] & 0x0F;
 							}
 							break;
+						default: throw new Exception( "PTMD: BPP switch (paletted), shouldn't be here." );
 					}
 
 					if ( Greyscale ) {
@@ -125,11 +154,35 @@ namespace HyoutaTools.LastRanker {
 						a = PaletteBytes[idx * 4 + 3];
 					}
 				} else {
-					int col = ImageBytes[i * 2] | ImageBytes[i * 2 + 1] << 8;
-					r = (byte)( ( col & 0x1F ) << 3 );
-					g = (byte)( ( ( col >> 5 ) & 0x1F ) << 3 );
-					b = (byte)( ( ( col >> 10 ) & 0x1F ) << 3 );
-					a = ( ( col >> 15 ) & 0x01 ) == 0 ? (byte)0x00 : (byte)0xFF;
+					int col;
+					switch ( BitPerPixel ) {
+						case 16:
+							col = ImageBytes[i * 2] | ImageBytes[i * 2 + 1] << 8;
+							r = (byte)( ( col & 0x1F ) << 3 );
+							g = (byte)( ( ( col >> 5 ) & 0x1F ) << 3 );
+							b = (byte)( ( ( col >> 10 ) & 0x1F ) << 3 );
+							a = ( ( col >> 15 ) & 0x01 ) == 0 ? (byte)0x00 : (byte)0xFF;
+							break;
+						case 32:
+							r = ImageBytes[i * 4];
+							g = ImageBytes[i * 4 + 1];
+							b = ImageBytes[i * 4 + 2];
+							a = ImageBytes[i * 4 + 3];
+							break;
+						case 8: // no clue if this is right
+							r = g = b = ImageBytes[i];
+							a = 0xFF;
+							break;
+						case 4: // no clue if this is right
+							if ( i % 2 == 1 ) {
+								r = g = b = (byte)( ( ( ImageBytes[i / 2] >> 4 ) & 0x0F ) << 4 );
+							} else {
+								r = g = b = (byte)( ( ImageBytes[i / 2] & 0x0F ) << 4 );
+							}
+							a = 0xFF;
+							break;
+						default: throw new Exception( "PTMD: BPP switch, shouldn't be here." );
+					}
 				}
 
 				int CurrentBlock = i / wh;
