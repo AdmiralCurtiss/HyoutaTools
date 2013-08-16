0465ad33a4b0aaf31ec18fbf270a81f7ea08c9a6
Admiral H. Curtiss <pikachu025@gmail.com>
2013-08-16 18:15:13 +0200

    Utility function to make inserting GraceNote entries into databases less annoying and copy-paste-y.
    Probably gonna fix up the old code to use this too, just so I don't have
    several mostly identical functions around...

--- GraceNote/GraceNoteDatabaseEntry.cs
+++ GraceNote/GraceNoteDatabaseEntry.cs
@@ -9,12 +9,27 @@ namespace HyoutaTools.GraceNote {
 
 		public string TextEN;
 		public string TextJP;
+		public string Comment;
 		public int ID;
 		public int JPID;
 		public int Status;
 
+		public int PointerRef;
+		public string IdentifyString;
+		public int IdentifyPointerRef;
+
 		public int NewLineCount;
 		public bool NewLineAtEnd;
+
+		public GraceNoteDatabaseEntry( string TextJP, string TextEN, string Comment, int Status, int PointerRef, string IdentifyString, int IdentifyPointerRef ) {
+			this.TextJP = TextJP;
+			this.TextEN = TextEN;
+			this.Comment = Comment;
+			this.Status = Status;
+			this.PointerRef = PointerRef;
+			this.IdentifyString = IdentifyString;
+			this.IdentifyPointerRef = IdentifyPointerRef;
+		}
 
 		public static GraceNoteDatabaseEntry[] GetAllEntriesFromDatabase( String ConnectionString, String GracesJapaneseConnectionString ) {
 			List<GraceNoteDatabaseEntry> Entries = new List<GraceNoteDatabaseEntry>();
@@ -76,6 +91,104 @@ namespace HyoutaTools.GraceNote {
 			Connection.Close();
 
 			return Entries.ToArray();
+		}
+
+		/// <summary>Utility Function to insert Entries into a GraceNote database. Usually used when ripping text from a game file.</summary>
+		/// <param name="Entries">The Entries to insert. Required values in the entries are: TextJP, TextEN, Comment, Status, PointerRef, IdentifyString, and IdentifyPointerRef. The rest is filled automatically.</param>
+		public static void InsertSQL( GraceNoteDatabaseEntry[] Entries, String ConnectionString, String ConnectionStringGracesJapanese ) {
+			SQLiteConnection Connection = new SQLiteConnection( ConnectionString );
+			SQLiteConnection ConnectionGracesJapanese = new SQLiteConnection( ConnectionStringGracesJapanese );
+			Connection.Open();
+			ConnectionGracesJapanese.Open();
+
+			using ( SQLiteTransaction Transaction = Connection.BeginTransaction() )
+			using ( SQLiteTransaction TransactionGracesJapanese = ConnectionGracesJapanese.BeginTransaction() )
+			using ( SQLiteCommand CommandInsertEntry = new SQLiteCommand( Connection ) )
+			using ( SQLiteCommand CommandInsertJapanese = new SQLiteCommand( ConnectionGracesJapanese ) )
+			using ( SQLiteCommand CommandGetMaxJapaneseID = new SQLiteCommand( ConnectionGracesJapanese ) )
+			using ( SQLiteCommand CommandSearchJapanese = new SQLiteCommand( ConnectionGracesJapanese ) ) {
+				SQLiteParameter JapaneseIDParam = new SQLiteParameter();
+				SQLiteParameter JapaneseParam = new SQLiteParameter();
+
+				SQLiteParameter EnglishIDParam = new SQLiteParameter();
+				SQLiteParameter StringIDParam = new SQLiteParameter();
+				SQLiteParameter EnglishParam = new SQLiteParameter();
+				SQLiteParameter CommentParam = new SQLiteParameter();
+				SQLiteParameter EnglishStatusParam = new SQLiteParameter();
+				SQLiteParameter PointerRefParam = new SQLiteParameter();
+				SQLiteParameter IdentifyStringParam = new SQLiteParameter();
+				SQLiteParameter IdentifyPointerRefParam = new SQLiteParameter();
+				SQLiteParameter UpdatedTimestampParam = new SQLiteParameter();
+
+				SQLiteParameter JapaneseSearchParam = new SQLiteParameter();
+
+
+				CommandInsertJapanese.CommandText = "INSERT INTO Japanese (ID, string, debug) VALUES (?, ?, 0)";
+				CommandInsertJapanese.Parameters.Add( JapaneseIDParam );
+				CommandInsertJapanese.Parameters.Add( JapaneseParam );
+
+				CommandInsertEntry.CommandText = "INSERT INTO Text (ID, StringID, english, comment, updated, status, PointerRef, IdentifyString, IdentifyPointerRef, UpdatedBy, UpdatedTimestamp)"
+														+ " VALUES (?,  ?,        ?,       ?,       0,       ?,      ?,          ?,              ?,                  \"HyoutaTools\", ?)";
+				CommandInsertEntry.Parameters.Add( EnglishIDParam );
+				CommandInsertEntry.Parameters.Add( StringIDParam );
+				CommandInsertEntry.Parameters.Add( EnglishParam );
+				CommandInsertEntry.Parameters.Add( CommentParam );
+				CommandInsertEntry.Parameters.Add( EnglishStatusParam );
+				CommandInsertEntry.Parameters.Add( PointerRefParam );
+				CommandInsertEntry.Parameters.Add( IdentifyStringParam );
+				CommandInsertEntry.Parameters.Add( IdentifyPointerRefParam );
+				CommandInsertEntry.Parameters.Add( UpdatedTimestampParam );
+
+				CommandGetMaxJapaneseID.CommandText = "SELECT MAX(ID)+1 FROM Japanese";
+
+				CommandSearchJapanese.CommandText = "SELECT ID FROM Japanese WHERE string = ? AND debug = 0";
+				CommandSearchJapanese.Parameters.Add( JapaneseSearchParam );
+
+				int JPID;
+				object JPMaxIDObject = CommandGetMaxJapaneseID.ExecuteScalar();
+				int JPMaxID;
+				try {
+					JPMaxID = Int32.Parse( JPMaxIDObject.ToString() );
+				} catch ( System.FormatException ) {
+					// there's no ID in the database, just start with 0
+					JPMaxID = 0;
+				}
+				int ENID = 1;
+
+				foreach ( GraceNoteDatabaseEntry e in Entries ) {
+					// fetch GracesJapanese ID or generate new & insert new text
+					JapaneseSearchParam.Value = e.TextJP;
+					object JPIDobj = CommandSearchJapanese.ExecuteScalar();
+					if ( JPIDobj != null ) {
+						JPID = (int)JPIDobj;
+					} else {
+						JPID = JPMaxID++;
+						JapaneseIDParam.Value = JPID;
+						JapaneseParam.Value = e.TextJP;
+						CommandInsertJapanese.ExecuteNonQuery();
+					}
+
+					// insert text into English table
+					EnglishIDParam.Value = ENID;
+					StringIDParam.Value = JPID;
+					EnglishParam.Value = e.TextEN;
+					CommentParam.Value = e.Comment;
+					EnglishStatusParam.Value = e.Status;
+					PointerRefParam.Value = e.PointerRef;
+					IdentifyStringParam.Value = e.IdentifyString;
+					IdentifyPointerRefParam.Value = e.IdentifyPointerRef;
+					UpdatedTimestampParam.Value = Util.DateTimeToUnixTime( DateTime.Now );
+					CommandInsertEntry.ExecuteNonQuery();
+
+					ENID++;
+				}
+				Transaction.Commit();
+				TransactionGracesJapanese.Commit();
+			}
+			ConnectionGracesJapanese.Close();
+			Connection.Close();
+
+			return;
 		}
 
 	}
