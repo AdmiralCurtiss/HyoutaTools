2b33e510bbe9a096b184710c4de2d022cb2643d3
Admiral H. Curtiss <pikachu025@gmail.com>
2013-08-16 19:25:40 +0200

    Fixing up old GraceNote importers.

--- GraceNote/LastRanker/StringImport.cs
+++ GraceNote/LastRanker/StringImport.cs
@@ -17,107 +17,21 @@ namespace HyoutaTools.GraceNote.LastRanker {
 			string GracesJapanese = args[2];
 			HyoutaTools.LastRanker.StringFile StringFile = new HyoutaTools.LastRanker.StringFile( System.IO.File.ReadAllBytes( InFile ) );
 			System.IO.File.WriteAllBytes( OutDatabase, Properties.Resources.gndb_template );
-			InsertSQL( StringFile, "Data Source=" + OutDatabase, "Data Source=" + GracesJapanese );
+
+			List<GraceNoteDatabaseEntry> Entries = new List<GraceNoteDatabaseEntry>( StringFile.Strings.Count );
+			foreach ( HyoutaTools.LastRanker.bscrString e in StringFile.Strings ) {
+				int status = 0;
+				if ( e.String.StartsWith( "func" ) || e.String.StartsWith( "■" ) || e.String.Trim() == "" ) {
+					status = -1;
+				}
+
+				GraceNoteDatabaseEntry gn = new GraceNoteDatabaseEntry( e.String, e.String, "", status, (int)e.Position, "", 0 );
+				Entries.Add( gn );
+			}
+
+			GraceNoteDatabaseEntry.InsertSQL( Entries.ToArray(), "Data Source=" + OutDatabase, "Data Source=" + GracesJapanese );
 
 			return 0;
 		}
-
-		public static bool InsertSQL( HyoutaTools.LastRanker.StringFile StringFile, String ConnectionString, String ConnectionStringGracesJapanese ) {
-			SQLiteConnection Connection = new SQLiteConnection( ConnectionString );
-			SQLiteConnection ConnectionGracesJapanese = new SQLiteConnection( ConnectionStringGracesJapanese );
-			Connection.Open();
-			ConnectionGracesJapanese.Open();
-
-			using ( SQLiteTransaction Transaction = Connection.BeginTransaction() )
-			using ( SQLiteTransaction TransactionGracesJapanese = ConnectionGracesJapanese.BeginTransaction() )
-			using ( SQLiteCommand Command = new SQLiteCommand( Connection ) )
-			using ( SQLiteCommand CommandGracesJapanese = new SQLiteCommand( ConnectionGracesJapanese ) )
-			using ( SQLiteCommand CommandJapaneseID = new SQLiteCommand( ConnectionGracesJapanese ) )
-			using ( SQLiteCommand CommandSearchJapanese = new SQLiteCommand( ConnectionGracesJapanese ) ) {
-				SQLiteParameter JapaneseIDParam = new SQLiteParameter();
-				SQLiteParameter JapaneseParam = new SQLiteParameter();
-				SQLiteParameter EnglishIDParam = new SQLiteParameter();
-				SQLiteParameter StringIDParam = new SQLiteParameter();
-				SQLiteParameter EnglishParam = new SQLiteParameter();
-				SQLiteParameter PointerRefParam = new SQLiteParameter();
-				SQLiteParameter IdentifyStringParam = new SQLiteParameter();
-				SQLiteParameter IdentifyPointerRefParam = new SQLiteParameter();
-				SQLiteParameter UpdatedTimestampParam = new SQLiteParameter();
-				SQLiteParameter JapaneseSearchParam = new SQLiteParameter();
-				SQLiteParameter EnglishStatusParam = new SQLiteParameter();
-
-
-				CommandGracesJapanese.CommandText = "INSERT INTO Japanese (ID, string, debug) VALUES (?, ?, 0)";
-				CommandGracesJapanese.Parameters.Add( JapaneseIDParam );
-				CommandGracesJapanese.Parameters.Add( JapaneseParam );
-
-				Command.CommandText = "INSERT INTO Text (ID, StringID, english, comment, updated, status, PointerRef, IdentifyString, IdentifyPointerRef, UpdatedBy, UpdatedTimestamp)"
-											 + " VALUES (?,  ?,        ?,       \"\",    0,       ?,      ?,          ?,              ?,                  \"HyoutaTools\", ?)";
-				Command.Parameters.Add( EnglishIDParam );
-				Command.Parameters.Add( StringIDParam );
-				Command.Parameters.Add( EnglishParam );
-				Command.Parameters.Add( EnglishStatusParam );
-				Command.Parameters.Add( PointerRefParam );
-				Command.Parameters.Add( IdentifyStringParam );
-				Command.Parameters.Add( IdentifyPointerRefParam );
-				Command.Parameters.Add( UpdatedTimestampParam );
-
-				CommandJapaneseID.CommandText = "SELECT MAX(ID)+1 FROM Japanese";
-
-				CommandSearchJapanese.CommandText = "SELECT ID FROM Japanese WHERE string = ? AND debug = 0";
-				CommandSearchJapanese.Parameters.Add( JapaneseSearchParam );
-
-				int JPID;
-				object JPMaxIDObject = CommandJapaneseID.ExecuteScalar();
-				int JPMaxID;
-				try {
-					JPMaxID = Int32.Parse( JPMaxIDObject.ToString() );
-				} catch ( System.FormatException ) {
-					// there's no ID in the database, just start with 0
-					JPMaxID = 0;
-				}
-				int ENID = 1;
-
-				foreach ( HyoutaTools.LastRanker.bscrString e in StringFile.Strings ) {
-					// fetch GracesJapanese ID or generate new & insert new text
-					JapaneseSearchParam.Value = e.String;
-					object JPIDobj = CommandSearchJapanese.ExecuteScalar();
-					if ( JPIDobj != null ) {
-						JPID = (int)JPIDobj;
-					} else {
-						JPID = JPMaxID++;
-						JapaneseIDParam.Value = JPID;
-						JapaneseParam.Value = e.String;
-						CommandGracesJapanese.ExecuteNonQuery();
-					}
-
-					// insert text into English table
-					EnglishIDParam.Value = ENID;
-					StringIDParam.Value = JPID;
-					EnglishParam.Value = e.String;
-					int status = 0;
-
-					if ( e.String.StartsWith( "func" ) || e.String.StartsWith( "■" ) || e.String.Trim() == "" ) {
-						status = -1;
-					}
-
-					EnglishStatusParam.Value = status;
-					PointerRefParam.Value = e.Position;
-					IdentifyStringParam.Value = "";
-					IdentifyPointerRefParam.Value = 0;
-					UpdatedTimestampParam.Value = Util.DateTimeToUnixTime( DateTime.Now );
-					Command.ExecuteNonQuery();
-
-					ENID++;
-				}
-				Transaction.Commit();
-				TransactionGracesJapanese.Commit();
-			}
-			ConnectionGracesJapanese.Close();
-			Connection.Close();
-
-			return true;
-		}
-
 	}
 }
--- GraceNote/XilliaScriptFileDump/Program.cs
+++ GraceNote/XilliaScriptFileDump/Program.cs
@@ -20,125 +20,26 @@ namespace HyoutaTools.GraceNote.XilliaScriptFileDump {
 
 	class Program {
 		public static int Execute( List<string> args ) {
-			if ( args.Count != 4 ) {
-				Console.WriteLine( "Usage: XilliaScript_GraceNote XilliaScriptFile NewDBFile TemplateDBFile GracesJapanese" );
+			if ( args.Count != 3 ) {
+				Console.WriteLine( "Usage: XilliaScript_GraceNote XilliaScriptFile NewDBFile GracesJapanese" );
 				return -1;
 			}
-
-			// templateDB must contain:
-			/***
-			 * CREATE TABLE Text(ID int primary key, StringID int, english text, comment text, updated tinyint, status tinyint, PointerRef integer, IdentifyString text, IdentifyPointerRef integer);
-			 ***/
-
-			//*
 			String Filename = args[0];
 			String NewDB = args[1];
-			String TemplateDB = args[2];
-			String GracesDB = args[3];
-			//*/
-
-			/*
-			String Filename = @"c:\Users\Georg\Downloads\Xillia Script files\69753.SDBJPN";
-			String NewDB = @"c:\Users\Georg\Downloads\Xillia Script files\X69753";
-			String TemplateDB = @"c:\Users\Georg\Downloads\Xillia Script files\XTemplate";
-			String GracesDB = @"c:\Users\Georg\Downloads\Xillia Script files\GracesJapanese";
-			//*/
+			String GracesDB = args[2];
 
 			XilliaScriptFile XSF = new XilliaScriptFile( Filename );
-			System.IO.File.Copy( TemplateDB, NewDB );
-			InsertSQL( XSF, "Data Source=" + NewDB, "Data Source=" + GracesDB );
+			System.IO.File.WriteAllBytes( NewDB, Properties.Resources.gndb_template );
 
-			//System.IO.File.WriteAllBytes(@"C:\TROPHY\newTrophyConf.trp", Encoding.UTF8.GetBytes(TROPSFM.ExportTropSFM(true)));
-			//System.IO.File.WriteAllBytes(@"C:\TROPHY\newTrophy.trp", Encoding.UTF8.GetBytes(TROPSFM.ExportTropSFM(false)));
+			List<GraceNoteDatabaseEntry> Entries = new List<GraceNoteDatabaseEntry>( XSF.TextList.Count );
+			foreach ( XS entry in XSF.TextList ) {
+				GraceNoteDatabaseEntry gn = new GraceNoteDatabaseEntry( entry.Text, entry.Text, "", 0, entry.PointerText, entry.IDString, entry.PointerIDString );
+				Entries.Add( gn );
+			}
+
+			GraceNoteDatabaseEntry.InsertSQL( Entries.ToArray(), "Data Source=" + NewDB, "Data Source=" + GracesDB );
 
 			return 0;
-		}
-
-
-
-
-
-
-
-		public static bool InsertSQL( XilliaScriptFile XSF, String ConnectionString, String ConnectionStringGracesJapanese ) {
-			SQLiteConnection Connection = new SQLiteConnection( ConnectionString );
-			SQLiteConnection ConnectionGracesJapanese = new SQLiteConnection( ConnectionStringGracesJapanese );
-			Connection.Open();
-			ConnectionGracesJapanese.Open();
-
-			using ( SQLiteTransaction Transaction = Connection.BeginTransaction() )
-			using ( SQLiteTransaction TransactionGracesJapanese = ConnectionGracesJapanese.BeginTransaction() )
-			using ( SQLiteCommand Command = new SQLiteCommand( Connection ) )
-			using ( SQLiteCommand CommandGracesJapanese = new SQLiteCommand( ConnectionGracesJapanese ) )
-			using ( SQLiteCommand CommandJapaneseID = new SQLiteCommand( ConnectionGracesJapanese ) )
-			using ( SQLiteCommand CommandSearchJapanese = new SQLiteCommand( ConnectionGracesJapanese ) ) {
-				SQLiteParameter JapaneseIDParam = new SQLiteParameter();
-				SQLiteParameter JapaneseParam = new SQLiteParameter();
-				SQLiteParameter EnglishIDParam = new SQLiteParameter();
-				SQLiteParameter StringIDParam = new SQLiteParameter();
-				SQLiteParameter EnglishParam = new SQLiteParameter();
-				SQLiteParameter PointerRefParam = new SQLiteParameter();
-				SQLiteParameter JapaneseSearchParam = new SQLiteParameter();
-				SQLiteParameter EnglishStatusParam = new SQLiteParameter();
-				SQLiteParameter IdentifyStringParam = new SQLiteParameter();
-				SQLiteParameter IdentifyPointerParam = new SQLiteParameter();
-
-
-				CommandGracesJapanese.CommandText = "INSERT INTO Japanese (ID, string, debug) VALUES (?, ?, 0)";
-				CommandGracesJapanese.Parameters.Add( JapaneseIDParam );
-				CommandGracesJapanese.Parameters.Add( JapaneseParam );
-
-				Command.CommandText = "INSERT INTO Text (ID, StringID, english, comment, updated, status, PointerRef, IdentifyString, IdentifyPointerRef) VALUES (?, ?, ?, \"\", 0, ?, ?, ?, ?)";
-				Command.Parameters.Add( EnglishIDParam );
-				Command.Parameters.Add( StringIDParam );
-				Command.Parameters.Add( EnglishParam );  // Line.SENG
-				Command.Parameters.Add( EnglishStatusParam );
-				Command.Parameters.Add( PointerRefParam ); // Line.Location
-				Command.Parameters.Add( IdentifyStringParam );
-				Command.Parameters.Add( IdentifyPointerParam );
-
-				CommandJapaneseID.CommandText = "SELECT MAX(ID)+1 FROM Japanese";
-
-				CommandSearchJapanese.CommandText = "SELECT ID FROM Japanese WHERE string = ? AND debug = 0";
-				CommandSearchJapanese.Parameters.Add( JapaneseSearchParam );
-
-				int JPID;
-				object JPMaxIDObject = CommandJapaneseID.ExecuteScalar();
-				int JPMaxID = Int32.Parse( JPMaxIDObject.ToString() ); // wtf why doesn't this work directly?
-				int ENID = 1;
-
-				foreach ( XS xs in XSF.TextList ) {
-					// fetch GracesJapanese ID or generate new & insert new text
-					JapaneseSearchParam.Value = xs.Text;
-					object JPIDobj = CommandSearchJapanese.ExecuteScalar();
-					if ( JPIDobj != null ) {
-						JPID = (int)JPIDobj;
-					} else {
-						JPID = JPMaxID++;
-						JapaneseIDParam.Value = JPID;
-						JapaneseParam.Value = xs.Text;
-						CommandGracesJapanese.ExecuteNonQuery();
-					}
-
-					// insert text into English table
-					EnglishIDParam.Value = ENID;
-					StringIDParam.Value = JPID;
-					EnglishParam.Value = xs.Text;
-					EnglishStatusParam.Value = 0;
-					PointerRefParam.Value = xs.PointerText; // == Trophy Name
-					IdentifyStringParam.Value = xs.IDString;
-					IdentifyPointerParam.Value = xs.PointerIDString;
-					Command.ExecuteNonQuery();
-
-					ENID++;
-				}
-				Transaction.Commit();
-				TransactionGracesJapanese.Commit();
-			}
-			ConnectionGracesJapanese.Close();
-			Connection.Close();
-
-			return true;
 		}
 	}
 }
