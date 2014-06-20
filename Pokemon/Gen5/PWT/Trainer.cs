61db372bb01ff8b6f5e750265332fce3818d6151
Admiral H. Curtiss <pikachu025@gmail.com>
2014-06-20 16:39:40 +0200

    Pokémon Gen 4 String decoding.

---
+++ Pokemon/Gen5/PWT/Trainer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HyoutaTools.Pokemon.Gen5.PWT {
+	public class Trainer {
+		public Trainer( byte[] data, int offset ) {
+
+		}
+	}
+}
