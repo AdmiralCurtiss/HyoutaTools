e391f9a2c01b1aad936d3dcd28fcf527a5453afb
Admiral H. Curtiss <pikachu025@gmail.com>
2014-07-15 21:40:14 +0200

    Vesperia.T8BTEMST: Parse Enemy data.

---
+++ Tales/Vesperia/T8BTEMST/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HyoutaTools.Tales.Vesperia.T8BTEMST {
+	public class Enemy {
+		public uint[] Data;
+		public float[] DataFloat;
+
+		public uint NameStringDicID;
+		public uint InGameID;
+		public string RefString;
+
+		public Enemy( System.IO.Stream stream, uint refStringStart ) {
+			Data = new uint[0x134 / 4];
+			DataFloat = new float[0x134 / 4];
+
+			for ( int i = 0; i < Data.Length; ++i ) {
+				Data[i] = stream.ReadUInt32().SwapEndian();
+				DataFloat[i] = Data[i].UIntToFloat();
+			}
+
+			NameStringDicID = Data[2];
+			InGameID = Data[5];
+			
+			uint refStringLocation = Data[6];
+			long pos = stream.Position;
+			stream.Position = refStringStart + refStringLocation;
+			RefString = stream.ReadAsciiNullterm();
+			stream.Position = pos;
+		}
+
+		public override string ToString() {
+			return RefString;
+		}
+	}
+}
---
+++ Tales/Vesperia/T8BTEMST/T8BTEMST.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace HyoutaTools.Tales.Vesperia.T8BTEMST {
+	public class T8BTEMST {
+		public T8BTEMST( String filename ) {
+			using ( Stream stream = new System.IO.FileStream( filename, FileMode.Open ) ) {
+				if ( !LoadFile( stream ) ) {
+					throw new Exception( "Loading T8BTEMST failed!" );
+				}
+			}
+		}
+
+		public T8BTEMST( Stream stream ) {
+			if ( !LoadFile( stream ) ) {
+				throw new Exception( "Loading T8BTEMST failed!" );
+			}
+		}
+
+		public List<Enemy> EnemyList;
+		public Dictionary<uint, Enemy> EnemyIdDict;
+
+		private bool LoadFile( Stream stream ) {
+			string magic = stream.ReadAscii( 8 );
+			uint enemyCount = stream.ReadUInt32().SwapEndian();
+			uint refStringStart = stream.ReadUInt32().SwapEndian();
+
+			EnemyList = new List<Enemy>( (int)enemyCount );
+			for ( uint i = 0; i < enemyCount; ++i ) {
+				Enemy s = new Enemy( stream, refStringStart );
+				EnemyList.Add( s );
+			}
+
+			EnemyIdDict = new Dictionary<uint, Enemy>( EnemyList.Count );
+			foreach ( Enemy e in EnemyList ) {
+				EnemyIdDict.Add( e.InGameID, e );
+			}
+
+			return true;
+		}
+	}
+}
