39d9a7b886d48e23aa345481e1e588b1153919c9
Admiral H. Curtiss <pikachu025@gmail.com>
2014-07-15 17:30:27 +0200

    Vesperia.T8BTSK: Parse Skill data.

---
+++ Tales/Vesperia/T8BTSK/Skill.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HyoutaTools.Tales.Vesperia.T8BTSK {
+	class Skill {
+		public uint ID;
+		public uint InGameID;
+		public uint NameStringDicID;
+		public uint DescStringDicID;
+
+		public uint Unknown7;
+		public uint LearnableByBitmask;
+		
+		public uint EquipCost;
+		public uint LearnCost;
+		public uint Category;
+		public uint SymbolValue;
+
+		public uint Unknown13;
+		public uint Unknown14;
+		public uint Unknown15;
+		public uint Unknown16;
+
+		public string RefString;
+
+		public Skill( System.IO.Stream stream, uint refStringStart ) {
+			uint entrySize = stream.ReadUInt32().SwapEndian();
+			ID = stream.ReadUInt32().SwapEndian();
+			InGameID = stream.ReadUInt32().SwapEndian();
+			uint refStringLocation = stream.ReadUInt32().SwapEndian();
+
+			NameStringDicID = stream.ReadUInt32().SwapEndian();
+			DescStringDicID = stream.ReadUInt32().SwapEndian();
+			Unknown7 = stream.ReadUInt32().SwapEndian();
+			LearnableByBitmask = stream.ReadUInt32().SwapEndian();
+
+			EquipCost = stream.ReadUInt32().SwapEndian();
+			LearnCost = stream.ReadUInt32().SwapEndian();
+			Category = stream.ReadUInt32().SwapEndian();
+			// Game sums up this value per category, then figures out the OVL-symbol from the totals
+			SymbolValue = stream.ReadUInt32().SwapEndian();
+
+			Unknown13 = stream.ReadUInt32().SwapEndian();
+			Unknown14 = stream.ReadUInt32().SwapEndian();
+			Unknown15 = stream.ReadUInt32().SwapEndian();
+			Unknown16 = stream.ReadUInt32().SwapEndian();
+
+			long pos = stream.Position;
+			stream.Position = refStringStart + refStringLocation;
+			RefString = stream.ReadAsciiNullterm();
+			stream.Position = pos;
+		}
+
+		public override string ToString() {
+			return RefString;
+		}
+	}
+}
---
+++ Tales/Vesperia/T8BTSK/T8BTSK.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace HyoutaTools.Tales.Vesperia.T8BTSK {
+	class T8BTSK {
+		public T8BTSK( String filename ) {
+			using ( Stream stream = new System.IO.FileStream( filename, FileMode.Open ) ) {
+				if ( !LoadFile( stream ) ) {
+					throw new Exception( "Loading T8BTSK failed!" );
+				}
+			}
+		}
+
+		public T8BTSK( Stream stream ) {
+			if ( !LoadFile( stream ) ) {
+				throw new Exception( "Loading T8BTSK failed!" );
+			}
+		}
+
+		public List<Skill> SkillList;
+
+		private bool LoadFile( Stream stream ) {
+			string magic = stream.ReadAscii( 8 );
+			uint skillCount = stream.ReadUInt32().SwapEndian();
+			uint refStringStart = stream.ReadUInt32().SwapEndian();
+
+			SkillList = new List<Skill>( (int)skillCount );
+			for ( uint i = 0; i < skillCount; ++i ) {
+				Skill s = new Skill( stream, refStringStart );
+				SkillList.Add( s );
+			}
+
+			return true;
+		}
+	}
+}
