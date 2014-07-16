36b84f3df2661337684f9c367945376764aa4940
Admiral H. Curtiss <pikachu025@gmail.com>
2014-07-16 19:07:51 +0200

    Vesperia.COOKDAT: Parse recipe data.

---
+++ Tales/Vesperia/COOKDAT/COOKDAT.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace HyoutaTools.Tales.Vesperia.COOKDAT {
+	public class COOKDAT {
+		public COOKDAT( String filename ) {
+			using ( Stream stream = new System.IO.FileStream( filename, FileMode.Open ) ) {
+				if ( !LoadFile( stream ) ) {
+					throw new Exception( "Loading COOKDAT failed!" );
+				}
+			}
+		}
+
+		public COOKDAT( Stream stream ) {
+			if ( !LoadFile( stream ) ) {
+				throw new Exception( "Loading COOKDAT failed!" );
+			}
+		}
+
+		public List<Recipe> RecipeList;
+
+		private bool LoadFile( Stream stream ) {
+			string magic = stream.ReadAscii( 8 );
+			uint unknown = stream.ReadUInt32().SwapEndian();
+			uint recipeCount = stream.ReadUInt32().SwapEndian();
+
+			RecipeList = new List<Recipe>( (int)recipeCount );
+			for ( uint i = 0; i < recipeCount; ++i ) {
+				Recipe r = new Recipe( stream );
+				RecipeList.Add( r );
+			}
+
+			return true;
+		}
+	}
+}
---
+++ Tales/Vesperia/COOKDAT/Recipe.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HyoutaTools.Tales.Vesperia.COOKDAT {
+	public class Recipe {
+		public uint[] Data;
+
+		public uint NameStringDicID;
+		public uint DescriptionStringDicID;
+		public uint EffectStringDicID;
+		public string RefString;
+
+		public Recipe( System.IO.Stream stream ) {
+			Data = new uint[0xCC / 4]; // + 0x20
+
+			for ( int i = 0; i < Data.Length; ++i ) {
+				Data[i] = stream.ReadUInt32().SwapEndian();
+			}
+			long pos = stream.Position;
+			RefString = stream.ReadAsciiNullterm();
+			stream.Position = pos + 0x20;
+
+			NameStringDicID = Data[1];
+			DescriptionStringDicID = Data[2];
+			EffectStringDicID = Data[3];
+		}
+
+		public override string ToString() {
+			return RefString;
+		}
+	}
+}
