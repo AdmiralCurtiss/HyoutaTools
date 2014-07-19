2ccd9fca11608dc44bfd3246dbb8b68b9aa44976
Admiral H. Curtiss <pikachu025@gmail.com>
2014-07-19 04:15:00 +0200

    Tales.Vesperia.BTLBDAT: Parse Battle Book data.

---
+++ Tales/Vesperia/BTLBDAT/BattleBookEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HyoutaTools.Tales.Vesperia.BTLBDAT {
+	public class BattleBookEntry {
+		public uint UnlockReferenceMaybe;
+		public uint NameStringDicId;
+		public uint TextStringDicId;
+
+		public BattleBookEntry( System.IO.Stream stream ) {
+			UnlockReferenceMaybe = stream.ReadUInt32().SwapEndian();
+			NameStringDicId = stream.ReadUInt32().SwapEndian();
+			TextStringDicId = stream.ReadUInt32().SwapEndian();
+			stream.DiscardBytes( 0x4 );
+		}
+	}
+}
