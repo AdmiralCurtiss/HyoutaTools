39f6a25107e2c3fe23cee8a10c56f58846da53bc
Admiral H. Curtiss <pikachu025@gmail.com>
2014-07-19 04:18:06 +0200

    Tales.Vesperia.T8BTTA: Parse Strategy data (sorta).

---
+++ Tales/Vesperia/T8BTTA/StrategySet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HyoutaTools.Tales.Vesperia.T8BTTA {
+	public class StrategySet {
+		public uint[] Data;
+		public uint NameStringDicID;
+		public uint DescStringDicID;
+
+		public string RefString;
+		public StrategySet( System.IO.Stream stream, uint refStringStart ) {
+			uint entrySize = stream.PeekUInt32().SwapEndian();
+			
+			Data = new uint[entrySize / 4];
+			for ( int i = 0; i < Data.Length; ++i ) {
+				Data[i] = stream.ReadUInt32().SwapEndian();
+			}
+
+			uint refStringLocation = Data[2];
+			NameStringDicID = Data[3];
+			DescStringDicID = Data[4];
+
+			long pos = stream.Position;
+			stream.Position = refStringStart + refStringLocation;
+			RefString = stream.ReadAsciiNullterm();
+			stream.Position = pos;
+		}
+
+		public override string ToString() {
+			return RefString;
+		}
+	}
+}
---
+++ Tales/Vesperia/T8BTTA/T8BTTA.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace HyoutaTools.Tales.Vesperia.T8BTTA {
+	public class T8BTTA {
+		public T8BTTA( String filename ) {
+			using ( Stream stream = new System.IO.FileStream( filename, FileMode.Open ) ) {
+				if ( !LoadFile( stream ) ) {
+					throw new Exception( "Loading T8BTTA failed!" );
+				}
+			}
+		}
+
+		public T8BTTA( Stream stream ) {
+			if ( !LoadFile( stream ) ) {
+				throw new Exception( "Loading T8BTTA failed!" );
+			}
+		}
+
+		public List<StrategySet> StrategySetList;
+		public List<StrategyOption> StrategyOptionList;
+
+		private bool LoadFile( Stream stream ) {
+			string magic = stream.ReadAscii( 8 );
+			uint strategySetCount = stream.ReadUInt32().SwapEndian();
+			uint strategyOptionCount = stream.ReadUInt32().SwapEndian();
+			uint refStringStart = stream.ReadUInt32().SwapEndian();
+
+			StrategySetList = new List<StrategySet>( (int)strategySetCount );
+			for ( uint i = 0; i < strategySetCount; ++i ) {
+				StrategySet ss = new StrategySet( stream, refStringStart );
+				StrategySetList.Add( ss );
+			}
+			StrategyOptionList = new List<StrategyOption>( (int)strategyOptionCount );
+			for ( uint i = 0; i < strategyOptionCount; ++i ) {
+				StrategyOption so = new StrategyOption( stream, refStringStart );
+				StrategyOptionList.Add( so );
+			}
+
+			return true;
+		}
+	}
+}
