780561a2f590d8ba264a3c552fb93fc37ed6be13
Admiral H. Curtiss <pikachu025@gmail.com>
2014-07-19 04:15:52 +0200

    Tales.Vesperia.FAMEDAT: Parse Title data.

---
+++ Tales/Vesperia/FAMEDAT/FAMEDAT.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace HyoutaTools.Tales.Vesperia.FAMEDAT {
+	public class FAMEDAT {
+		public FAMEDAT( String filename ) {
+			using ( Stream stream = new System.IO.FileStream( filename, FileMode.Open ) ) {
+				if ( !LoadFile( stream ) ) {
+					throw new Exception( "Loading FAMEDAT failed!" );
+				}
+			}
+		}
+
+		public FAMEDAT( Stream stream ) {
+			if ( !LoadFile( stream ) ) {
+				throw new Exception( "Loading FAMEDAT failed!" );
+			}
+		}
+
+		public List<Title> TitleList;
+
+		private bool LoadFile( Stream stream ) {
+			string magic = stream.ReadAscii( 8 );
+			uint unknown = stream.ReadUInt32().SwapEndian();
+			uint titleCount = stream.ReadUInt32().SwapEndian();
+
+			TitleList = new List<Title>( (int)titleCount );
+			for ( uint i = 0; i < titleCount; ++i ) {
+				Title t = new Title( stream );
+				TitleList.Add( t );
+			}
+
+			return true;
+		}
+	}
+}
---
+++ Tales/Vesperia/FAMEDAT/Title.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HyoutaTools.Tales.Vesperia.FAMEDAT {
+	public class Title {
+		public uint ID;
+		public uint NameStringDicID;
+		public uint DescStringDicID;
+		public uint Character;
+		public uint BunnyGuildPointsMaybe;
+
+		public string CostumeString;
+
+		public Title( System.IO.Stream stream ) {
+			ID = stream.ReadUInt32().SwapEndian();
+			NameStringDicID = stream.ReadUInt32().SwapEndian();
+			DescStringDicID = stream.ReadUInt32().SwapEndian();
+			Character = stream.ReadUInt32().SwapEndian();
+
+			CostumeString = stream.ReadAscii( 0x10 ).TrimNull();
+
+			BunnyGuildPointsMaybe = stream.ReadUInt32().SwapEndian();
+			stream.DiscardBytes( 0xC );
+		}
+	}
+}
