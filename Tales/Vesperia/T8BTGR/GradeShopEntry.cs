e8e698436997f114a4283517a2ada05475e85f85
Admiral H. Curtiss <pikachu025@gmail.com>
2014-07-19 04:17:02 +0200

    Tales.Vesperia.T8BTGR: Parse Grade Shop data.

---
+++ Tales/Vesperia/T8BTGR/GradeShopEntry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HyoutaTools.Tales.Vesperia.T8BTGR {
+	public class GradeShopEntry {
+		public uint ID;
+		public uint InGameID;
+
+		public uint NameStringDicID;
+		public uint DescStringDicID;
+		public uint GradeCost;
+		
+		public string RefString;
+		public GradeShopEntry( System.IO.Stream stream, uint refStringStart ) {
+			uint entrySize = stream.ReadUInt32().SwapEndian();
+			ID = stream.ReadUInt32().SwapEndian();
+			InGameID = stream.ReadUInt32().SwapEndian();
+			uint refStringLocation = stream.ReadUInt32().SwapEndian();
+
+			NameStringDicID = stream.ReadUInt32().SwapEndian();
+			DescStringDicID = stream.ReadUInt32().SwapEndian();
+			GradeCost = stream.ReadUInt32().SwapEndian();
+
+			long pos = stream.Position;
+			stream.Position = refStringStart + refStringLocation;
+			RefString = stream.ReadAsciiNullterm();
+			stream.Position = pos;
+		}
+
+		public override string ToString() {
+			return RefString;
+		}
+	}
+}
---
+++ Tales/Vesperia/T8BTGR/T8BTGR.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace HyoutaTools.Tales.Vesperia.T8BTGR {
+	public class T8BTGR {
+		public T8BTGR( String filename ) {
+			using ( Stream stream = new System.IO.FileStream( filename, FileMode.Open ) ) {
+				if ( !LoadFile( stream ) ) {
+					throw new Exception( "Loading T8BTGR failed!" );
+				}
+			}
+		}
+
+		public T8BTGR( Stream stream ) {
+			if ( !LoadFile( stream ) ) {
+				throw new Exception( "Loading T8BTGR failed!" );
+			}
+		}
+
+		public List<GradeShopEntry> GradeShopEntryList;
+
+		private bool LoadFile( Stream stream ) {
+			string magic = stream.ReadAscii( 8 );
+			uint entryCount = stream.ReadUInt32().SwapEndian();
+			uint refStringStart = stream.ReadUInt32().SwapEndian();
+
+			GradeShopEntryList = new List<GradeShopEntry>( (int)entryCount );
+			for ( uint i = 0; i < entryCount; ++i ) {
+				GradeShopEntry e = new GradeShopEntry( stream, refStringStart );
+				GradeShopEntryList.Add( e );
+			}
+
+			return true;
+		}
+	}
+}
