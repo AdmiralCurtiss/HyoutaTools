36a32424fb2cc96b5442f01dce2e6f3852ed0a08
Admiral H. Curtiss <pikachu025@gmail.com>
2014-07-31 21:07:07 +0200

    Tales.Vesperia.TOVNPCL: NPC File List parsing.

---
+++ Tales/Vesperia/TOVNPC/NpcFileReference.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HyoutaTools.Tales.Vesperia.TOVNPC {
+	public class NpcFileReference {
+		public string Map;
+		public string Filename;
+		public uint Filesize;
+
+		public NpcFileReference( System.IO.Stream stream, uint refStringStart ) {
+			uint refStringLocation1 = stream.ReadUInt32().SwapEndian();
+			uint refStringLocation2 = stream.ReadUInt32().SwapEndian();
+			Filesize = stream.ReadUInt32().SwapEndian();
+			stream.ReadUInt32();
+
+			long pos = stream.Position;
+			stream.Position = refStringStart + refStringLocation1;
+			Map = stream.ReadAsciiNullterm();
+			stream.Position = refStringStart + refStringLocation2;
+			Filename = stream.ReadAsciiNullterm();
+			stream.Position = pos;
+		}
+
+		public override string ToString() {
+			return Map + " / " + Filename + " / " + Filesize;
+		}
+	}
+}
---
+++ Tales/Vesperia/TOVNPC/TOVNPCL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace HyoutaTools.Tales.Vesperia.TOVNPC {
+	public class TOVNPCL {
+		// NPC file list
+		public TOVNPCL( String filename ) {
+			using ( Stream stream = new System.IO.FileStream( filename, FileMode.Open ) ) {
+				if ( !LoadFile( stream ) ) {
+					throw new Exception( "Loading TOVNPCL failed!" );
+				}
+			}
+		}
+
+		public TOVNPCL( Stream stream ) {
+			if ( !LoadFile( stream ) ) {
+				throw new Exception( "Loading TOVNPCL failed!" );
+			}
+		}
+
+		public List<NpcFileReference> NpcFileList;
+
+		private bool LoadFile( Stream stream ) {
+			string magic = stream.ReadAscii( 8 );
+			uint fileSize = stream.ReadUInt32().SwapEndian();
+			uint dataStart = stream.ReadUInt32().SwapEndian();
+			uint dataCount = stream.ReadUInt32().SwapEndian();
+			uint refStringStart = stream.ReadUInt32().SwapEndian();
+
+			stream.Position = dataStart;
+			NpcFileList = new List<NpcFileReference>( (int)dataCount );
+			for ( uint i = 0; i < dataCount; ++i ) {
+				NpcFileReference n = new NpcFileReference( stream, refStringStart );
+				NpcFileList.Add( n );
+			}
+
+			return true;
+		}
+	}
+}
