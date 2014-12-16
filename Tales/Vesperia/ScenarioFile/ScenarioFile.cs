b38e85947aeefaa2a1bb83b16cb929152a453328
Admiral H. Curtiss <pikachu025@gmail.com>
2014-12-16 02:04:34 +0100

    Tales.Vesperia.ScenarioFile: Add simple class for reading scenario strings.

---
+++ Tales/Vesperia/ScenarioFile/ScenarioFile.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace HyoutaTools.Tales.Vesperia.ScenarioFile {
+	public class ScenarioFile {
+		public ScenarioFile( String filename ) {
+			using ( Stream stream = new System.IO.FileStream( filename, FileMode.Open ) ) {
+				if ( !LoadFile( stream ) ) {
+					throw new Exception( "Loading ScenarioFile failed!" );
+				}
+			}
+		}
+
+		public ScenarioFile( Stream stream ) {
+			if ( !LoadFile( stream ) ) {
+				throw new Exception( "Loading ScenarioFile failed!" );
+			}
+		}
+
+		public List<ScenarioFileEntry> EntryList;
+
+		private bool LoadFile( Stream stream ) {
+			uint magic = stream.ReadUInt32().SwapEndian();
+			uint headerSize = stream.ReadUInt32().SwapEndian();
+			stream.ReadUInt32().SwapEndian();
+			uint textStart = stream.ReadUInt32().SwapEndian();
+
+			stream.ReadUInt32().SwapEndian();
+			stream.ReadUInt32().SwapEndian();
+			uint textLength = stream.ReadUInt32().SwapEndian();
+			stream.ReadUInt32().SwapEndian();
+
+			EntryList = FindText( stream, textStart );
+
+			return true;
+		}
+
+		private static List<ScenarioFileEntry> FindText( Stream stream, uint textStart ) {
+			var list = new List<ScenarioFileEntry>();
+
+			while ( stream.Position < textStart ) {
+				uint identifyingBytes = stream.ReadUInt32();
+				if ( identifyingBytes == 0x18000C04 ) {
+					uint pos = (uint)stream.Position;
+
+					uint pointerToText = stream.ReadUInt32().SwapEndian() + textStart;
+					
+					stream.Position = pointerToText;
+					stream.ReadUInt32().SwapEndian();
+					stream.ReadUInt32().SwapEndian();
+					uint jpNamePtr = stream.ReadUInt32().SwapEndian();
+					uint jpTextPtr = stream.ReadUInt32().SwapEndian();
+					uint enNamePtr = stream.ReadUInt32().SwapEndian();
+					uint enTextPtr = stream.ReadUInt32().SwapEndian();
+					stream.Position = jpNamePtr + textStart; string jpName = stream.ReadShiftJisNullterm();
+					stream.Position = jpTextPtr + textStart; string jpText = stream.ReadShiftJisNullterm();
+					stream.Position = enNamePtr + textStart; string enName = stream.ReadShiftJisNullterm();
+					stream.Position = enTextPtr + textStart; string enText = stream.ReadShiftJisNullterm();
+
+					var entry = new ScenarioFileEntry() { Pointer = pos, JpName = jpName, EnName = enName, JpText = jpText, EnText = enText };
+					list.Add( entry );
+
+					stream.Position = pos + 4;
+				}
+			}
+
+			return list;
+		}
+	}
+}
---
+++ Tales/Vesperia/ScenarioFile/ScenarioFileEntry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HyoutaTools.Tales.Vesperia.ScenarioFile {
+	public class ScenarioFileEntry : IComparable<ScenarioFileEntry> {
+		public uint Pointer;
+
+		public string JpName;
+		public string JpText;
+		public string EnName;
+		public string EnText;
+
+		public override string ToString() {
+			return EnName + ": " + EnText;
+		}
+
+		public int CompareTo( ScenarioFileEntry other ) {
+			return Pointer.CompareTo( other.Pointer );
+		}
+	}
+}
