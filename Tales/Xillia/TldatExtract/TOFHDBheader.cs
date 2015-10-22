6c8244404e3033eeb66d891d997a6576bb83d19f
Admiral H. Curtiss <pikachu025@gmail.com>
2015-10-22 21:57:30 +0200

    Tales.Xillia.TLDAT: Make Endian-agnostic as Zestiria (PC) uses the same container format.

--- Tales/Xillia/TldatExtract/TOFHDBheader.cs
+++ Tales/Xillia/TldatExtract/TOFHDBheader.cs
@@ -16,6 +16,7 @@ namespace HyoutaTools.Tales.Xillia.TldatExtract {
 	public class TOFHDBheader {
 		public List<TOFHDBEntry> EntryList;
 		public UInt32 FileAmount;
+		public Util.Endianness Endian = Util.Endianness.BigEndian;
 
 		public TOFHDBheader( String Filename ) {
 			Load( System.IO.File.ReadAllBytes( Filename ) );
@@ -32,20 +33,28 @@ namespace HyoutaTools.Tales.Xillia.TldatExtract {
 		}
 
 		private bool Load( Byte[] Bytes ) {
-			FileAmount = Util.SwapEndian( BitConverter.ToUInt32( Bytes, 0xC ) );
+			UInt32 unknown = BitConverter.ToUInt32( Bytes, 0x8 );
+			// should be 0x20 or a similarly small number, use to detect endianness
+			if ( unknown < 0xFFFF ) {
+				Endian = Util.Endianness.LittleEndian;
+			} else {
+				Endian = Util.Endianness.BigEndian;
+			}
 
-			UInt32 FileListStart = 0x18 + Util.SwapEndian( BitConverter.ToUInt32( Bytes, 0x18 ) );
+			FileAmount = BitConverter.ToUInt32( Bytes, 0xC ).FromEndian( Endian );
+
+			UInt32 FileListStart = 0x18 + BitConverter.ToUInt32( Bytes, 0x18 ).FromEndian( Endian );
 
 			EntryList = new List<TOFHDBEntry>( (int)FileAmount );
 
 			for ( UInt32 i = 0; i < FileAmount; i++ ) {
 				TOFHDBEntry e = new TOFHDBEntry();
-				e.Filesize = Util.SwapEndian( BitConverter.ToUInt64( Bytes, (int)( FileListStart + ( i * 0x28 ) + 0x00 ) ) );
-				e.CompressedSize = Util.SwapEndian( BitConverter.ToUInt64( Bytes, (int)( FileListStart + ( i * 0x28 ) + 0x08 ) ) );
-				e.Offset = Util.SwapEndian( BitConverter.ToUInt64( Bytes, (int)( FileListStart + ( i * 0x28 ) + 0x10 ) ) );
-				e.Hash = Util.SwapEndian( BitConverter.ToUInt32( Bytes, (int)( FileListStart + ( i * 0x28 ) + 0x18 ) ) );
+				e.Filesize = BitConverter.ToUInt64( Bytes, (int)( FileListStart + ( i * 0x28 ) + 0x00 ) ).FromEndian( Endian );
+				e.CompressedSize = BitConverter.ToUInt64( Bytes, (int)( FileListStart + ( i * 0x28 ) + 0x08 ) ).FromEndian( Endian );
+				e.Offset = BitConverter.ToUInt64( Bytes, (int)( FileListStart + ( i * 0x28 ) + 0x10 ) ).FromEndian( Endian );
+				e.Hash = BitConverter.ToUInt32( Bytes, (int)( FileListStart + ( i * 0x28 ) + 0x18 ) ).FromEndian( Endian );
 				e.Extension = Util.TrimNull( Encoding.ASCII.GetString( Bytes, (int)( FileListStart + ( i * 0x28 ) + 0x1C ), 0x0A ) );
-				e.Unknown = Util.SwapEndian( BitConverter.ToUInt16( Bytes, (int)( FileListStart + ( i * 0x28 ) + 0x26 ) ) );
+				e.Unknown = BitConverter.ToUInt16( Bytes, (int)( FileListStart + ( i * 0x28 ) + 0x26 ) ).FromEndian( Endian );
 				EntryList.Add( e );
 			}
 
