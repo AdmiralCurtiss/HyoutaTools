9ccafd551ffd506a5022fe714ed6d44cb5441ad8
Admiral H. Curtiss <pikachu025@gmail.com>
2015-10-30 23:47:55 +0100

    Tales.Xillia.SDB: Add support for Zestiria's variants.

--- Tales/Xillia/SDB/SDB.cs
+++ Tales/Xillia/SDB/SDB.cs
@@ -13,7 +13,9 @@ namespace HyoutaTools.Tales.Xillia.SDB {
 	}
 
 	class SDB {
-		public List<SDBEntry> TextList;
+		public uint Type;
+		public List<uint> OtherData;
+        public List<SDBEntry> TextList;
 		public Util.Endianness Endian;
 
 		public SDB( String filename ) {
@@ -32,21 +34,39 @@ namespace HyoutaTools.Tales.Xillia.SDB {
 
 		private bool LoadFile( System.IO.Stream stream ) {
 			// not entirely sure if this is actually type information?
-			uint type = stream.ReadUInt32();
-			if ( type <= 0xFFFF ) {
+			uint Type = stream.ReadUInt32();
+			if ( Type <= 0xFFFF ) {
 				Endian = Util.Endianness.LittleEndian;
 			} else {
 				Endian = Util.Endianness.BigEndian;
-				type = type.SwapEndian();
-			}
-			if ( type != 0x08 ) {
-				System.Console.WriteLine( "Unknown SDB file type!" );
-				return false;
+				Type = Type.SwapEndian();
 			}
 
-			uint Amount = stream.ReadUInt32().FromEndian( Endian );
-			TextList = new List<SDBEntry>( (int)Amount );
-			for ( uint i = 0; i < Amount; ++i ) {
+			bool ToZ;
+			switch ( Type ) {
+				case 0x08: ToZ = false; break;
+				case 0x100: ToZ = true; break;
+				default: System.Console.WriteLine( "Unknown SDB file type!" ); return false;
+			}
+
+			if ( ToZ ) {
+				// seems to always be 0x0C
+				uint unknown = stream.ReadUInt32().FromEndian( Endian );
+			}
+
+			uint textAmount = stream.ReadUInt32().FromEndian( Endian );
+
+			if ( ToZ ) {
+				uint otherDataAmount = stream.ReadUInt32().FromEndian( Endian );
+				OtherData = new List<uint>( (int)otherDataAmount );
+				for ( uint i = 0; i < otherDataAmount; ++i ) {
+					OtherData.Add( stream.ReadUInt32().FromEndian( Endian ) );
+				}
+				uint textOffset = stream.ReadUInt32().FromEndian( Endian );
+			}
+
+			TextList = new List<SDBEntry>( (int)textAmount );
+			for ( uint i = 0; i < textAmount; ++i ) {
 				SDBEntry x = new SDBEntry();
 				x.PointerIDString = (uint)stream.Position + stream.ReadUInt32().FromEndian( Endian );
 				x.PointerText = (uint)stream.Position + stream.ReadUInt32().FromEndian( Endian );
