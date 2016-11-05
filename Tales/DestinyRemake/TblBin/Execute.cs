fdf1db6e1358bad5d5d3803cb65c13565d1eda34
Admiral H. Curtiss <pikachu025@gmail.com>
2016-11-05 22:05:21 +0100

    Print usage in Tales.ToDR.TblBin.

--- Tales/DestinyRemake/TblBin/Execute.cs
+++ Tales/DestinyRemake/TblBin/Execute.cs
@@ -6,6 +6,10 @@ using System.Text;
 namespace HyoutaTools.Tales.DestinyRemake.TblBin {
 	class Execute {
 		public static int Extract( List<string> args ) {
+			if ( args.Count != 2 ) {
+				Console.WriteLine( "Usage: DAT.TBL DAT.BIN" );
+				return -1;
+			}
 
 			string tblpath = args[0];
 			string binpath = args[1];
