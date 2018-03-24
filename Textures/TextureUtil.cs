74a21bdf269180902ff9e64e91a46003595b7bfe
Admiral H. Curtiss <pikachu025@gmail.com>
2018-03-25 00:19:57 +0100

    Add ColorFetchingIterator interface for more generic texture decoding.

---
+++ Textures/TextureUtil.cs
@@ -0,0 +1,25 @@
+﻿using HyoutaTools.Textures.ColorFetchingIterators;
+using HyoutaTools.Textures.PixelOrderIterators;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyoutaTools.Textures {
+	public static class TextureUtil {
+		public static Stream WriteSingleImageToPngStream( this IColorFetchingIterator colorFetchingIterator, IPixelOrderIterator pixelOrderIterator, uint width, uint height ) {
+			var bitmap = new System.Drawing.Bitmap( (int)width, (int)height );
+			foreach ( Color c in colorFetchingIterator ) {
+				bitmap.SetPixel( pixelOrderIterator.X, pixelOrderIterator.Y, c );
+				pixelOrderIterator.Next();
+			}
+
+			MemoryStream s = new MemoryStream();
+			bitmap.Save( s, System.Drawing.Imaging.ImageFormat.Png );
+			return s;
+		}
+	}
+}
