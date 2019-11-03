cc0fd2d51872d76f7bd84d0ea84f920c0ea2a933
Admiral H. Curtiss <pikachu025@gmail.com>
2019-11-03 19:08:36 +0100

    Add ColorFetcherGrey8Alpha8.

---
+++ Textures/ColorFetchingIterators/ColorFetcherGrey8Alpha8.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HyoutaUtils;
+
+namespace HyoutaTools.Textures.ColorFetchingIterators {
+	public class ColorFetcherGrey8Alpha8 : IColorFetchingIterator {
+		private Stream SourceStream;
+		private long PixelCount;
+		private EndianUtils.Endianness Endian;
+
+		public ColorFetcherGrey8Alpha8( Stream stream, long width, long height, EndianUtils.Endianness endian ) {
+			SourceStream = stream;
+			PixelCount = width * height;
+			Endian = endian;
+		}
+
+		public IEnumerator<Color> GetEnumerator() {
+			for ( long i = 0; i < PixelCount; ++i ) {
+				yield return ColorFromGrey8Alpha8( SourceStream.ReadUInt16().FromEndian( Endian ) );
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() {
+			return GetEnumerator();
+		}
+
+		public static Color ColorFromGrey8Alpha8( ushort color ) {
+			int a = ( color >> 8 ) & 0xFF;
+			int g = color & 0xFF;
+			return Color.FromArgb( a, g, g, g );
+		}
+	}
+}
