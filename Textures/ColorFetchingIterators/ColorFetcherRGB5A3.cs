c50a02891d1025c422894e534ae979066ad44bb0
Admiral H. Curtiss <pikachu025@gmail.com>
2019-11-03 20:42:16 +0100

    Add ColorFetcherRGB5A3.

---
+++ Textures/ColorFetchingIterators/ColorFetcherRGB5A3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HyoutaUtils;
+
+namespace HyoutaTools.Textures.ColorFetchingIterators {
+	public class ColorFetcherRGB5A3 : IColorFetchingIterator {
+		private Stream SourceStream;
+		private long PixelCount;
+		private EndianUtils.Endianness Endian;
+
+		public ColorFetcherRGB5A3( Stream stream, long width, long height, EndianUtils.Endianness endian ) {
+			SourceStream = stream;
+			PixelCount = width * height;
+			Endian = endian;
+		}
+
+		public IEnumerator<Color> GetEnumerator() {
+			for ( long i = 0; i < PixelCount; ++i ) {
+				yield return ColorFromRGB5A3( SourceStream.ReadUInt16().FromEndian( Endian ) );
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() {
+			return GetEnumerator();
+		}
+
+		public static Color ColorFromRGB5A3( ushort color ) {
+			bool is555 = ( color & 0x8000 ) > 0;
+			if ( is555 ) {
+				int b = (int)( ( ( color & 0x001F )       ) << 3 );
+				int g = (int)( ( ( color & 0x03E0 ) >>  5 ) << 3 );
+				int r = (int)( ( ( color & 0x7C00 ) >> 10 ) << 3 );
+				return Color.FromArgb( r, g, b );
+			} else {
+				int b = (int)( ( ( color & 0x000F )       ) << 4 );
+				int g = (int)( ( ( color & 0x00F0 ) >> 4  ) << 4 );
+				int r = (int)( ( ( color & 0x0F00 ) >> 8  ) << 4 );
+				int a = (int)( ( ( color & 0x7000 ) >> 12 ) << 5 );
+				return Color.FromArgb( a, r, g, b );
+			}
+		}
+	}
+}
