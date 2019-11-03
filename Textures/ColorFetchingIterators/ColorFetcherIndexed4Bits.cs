ba3e4877cbc41d8725a3aa30f987f561041e6f3d
Admiral H. Curtiss <pikachu025@gmail.com>
2019-11-03 19:08:51 +0100

    Add ColorFetcherIndexed4Bits.

---
+++ Textures/ColorFetchingIterators/ColorFetcherIndexed4Bits.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HyoutaUtils;
+
+namespace HyoutaTools.Textures.ColorFetchingIterators {
+	public class ColorFetcherIndexed4Bits : IColorFetchingIterator {
+		private Stream SourceStream;
+		private long PixelCount;
+		private Color[] colors;
+
+		public ColorFetcherIndexed4Bits( Stream stream, long width, long height, IColorFetchingIterator colorConverter ) {
+			SourceStream = stream;
+			PixelCount = width * height;
+			colors = colorConverter.ToArray();
+		}
+
+		public IEnumerator<Color> GetEnumerator() {
+			for ( long i = 0; i < PixelCount / 2; ++i ) {
+				byte b = SourceStream.ReadUInt8();
+				yield return colors[( b >> 4 ) & 0x0F];
+				yield return colors[b & 0x0F];
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() {
+			return GetEnumerator();
+		}
+	}
+}
