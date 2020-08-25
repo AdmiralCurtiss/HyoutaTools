ee3186dbfc2b846e77380c956c264825d47d5c4f
Admiral H. Curtiss <pikachu025@gmail.com>
2020-08-25 16:15:16 +0200

    iPck can have empty entries.

--- Tales/Graces/iPck.cs
+++ Tales/Graces/iPck.cs
@@ -26,15 +26,32 @@ namespace HyoutaTools.Tales.Graces {
 				uint[] offsets = s.ReadUInt32Array(count, e);
 
 				s.Position = dataStart;
+				long dataEnd = s.Length - dataStart;
 				Data = new List<byte[]>((int)count);
 				for (uint i = 0; i < count; ++i) {
-					long start = dataStart + offsets[i];
-					long end = (i == (count - 1)) ? s.Length : (dataStart + offsets[i + 1]);
-					long len = end - start;
-					s.Position = start;
-					Data.Add(s.ReadUInt8Array(len));
+					long start = offsets[i];
+					if (start == 0xffffffffu) {
+						Data.Add(null);
+					} else {
+						long end = FindFileEnd(offsets, i, dataEnd);
+						long len = end - start;
+						s.Position = dataStart + start;
+						Data.Add(s.ReadUInt8Array(len));
+					}
 				}
 			}
+		}
+
+		private static long FindFileEnd(uint[] offsets, uint index, long eof) {
+			++index;
+			while (index < offsets.Length) {
+				uint end = offsets[index];
+				if (end != 0xffffffffu) {
+					return end;
+				}
+				++index;
+			}
+			return eof;
 		}
 
 		public DuplicatableStream WriteToStream(EndianUtils.Endianness e = EndianUtils.Endianness.BigEndian) {
@@ -45,11 +62,17 @@ namespace HyoutaTools.Tales.Graces {
 				ms.WriteUInt32((uint)(0x10 + Data.Count * 4), e);
 				uint datapos = 0;
 				for (int i = 0; i < Data.Count; ++i) {
-					ms.WriteUInt32(datapos, e);
-					datapos += (uint)Data[i].Length;
+					if (Data[i] != null) {
+						ms.WriteUInt32(datapos, e);
+						datapos += (uint)Data[i].Length;
+					} else {
+						ms.WriteUInt32(0xffffffffu, e);
+					}
 				}
 				for (int i = 0; i < Data.Count; ++i) {
-					ms.Write(Data[i]);
+					if (Data[i] != null) {
+						ms.Write(Data[i]);
+					}
 				}
 				return ms.CopyToByteArrayStreamAndDispose();
 			}
