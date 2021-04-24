168083f5bea112b7a3cda04d0e371e993b07f546
Admiral H. Curtiss <pikachu025@gmail.com>
2021-04-24 20:48:24 +0200

    Add CPK compression and decompression tools.

--- Tales/CPK/Program.cs
+++ Tales/CPK/Program.cs
@@ -9,13 +9,14 @@ using System.Threading.Tasks;
 namespace HyoutaTools.Tales.CPK {
 	public class Program {
 		public static void PrintExtractUsage() {
-			Console.WriteLine("Usage: file.cpk [OutputDirectory] [--recursive]");
+			Console.WriteLine("Usage: [--recursive] [--no-decompress] file.cpk [OutputDirectory]");
 		}
 
 		public static int Extract(List<string> args) {
 			string inpath = null;
 			string outpath = null;
 			bool recursive = false;
+			bool decompress = true;
 
 			try {
 				for (int i = 0; i < args.Count; ++i) {
@@ -23,6 +24,9 @@ namespace HyoutaTools.Tales.CPK {
 						case "-r":
 						case "--recursive":
 							recursive = true;
+							break;
+						case "--no-decompress":
+							decompress = false;
 							break;
 						default:
 							if (inpath == null) { inpath = args[i]; } else if (outpath == null) { outpath = args[i]; } else { PrintExtractUsage(); return -1; }
@@ -44,12 +48,21 @@ namespace HyoutaTools.Tales.CPK {
 			}
 
 			CpkContainer cpk = new CpkContainer(new HyoutaUtils.Streams.DuplicatableFileStream(inpath));
-			DoExtract(cpk, outpath, recursive);
+			DoExtract(cpk, outpath, recursive, decompress);
 
 			return 0;
 		}
 
-		private static void DoExtract(CpkContainer cpk, string outdir, bool recursive) {
+		private static HyoutaPluginBase.DuplicatableStream GetFile(CpkContainer cpk, long index, bool decompress) {
+			if (decompress) {
+				return cpk.GetChildByIndex(index).AsFile.DataStream;
+			} else {
+				var entry = cpk.GetEntryByIndex(index);
+				return new HyoutaUtils.Streams.PartialStream(cpk.DuplicateStream(), entry.file_offset, entry.file_size);
+			}
+		}
+
+		private static void DoExtract(CpkContainer cpk, string outdir, bool recursive, bool decompress) {
 			for (long i = 0; i < cpk.toc_entries; ++i) {
 				CpkContainer.Entry entry = cpk.GetEntryByIndex(i);
 				if (entry == null) {
@@ -64,18 +77,94 @@ namespace HyoutaTools.Tales.CPK {
 					} else {
 						subcpk = new CpkContainer(cpk.DuplicateStream(), entry.file_offset);
 					}
-					DoExtract(subcpk, Path.Combine(outdir, name + ".ext"), recursive);
+					DoExtract(subcpk, Path.Combine(outdir, name + ".ext"), recursive, decompress);
 					continue;
 				}
 
 				string outpath = Path.Combine(outdir, name);
 				Console.WriteLine("Extracting {0}...", outpath);
 				Directory.CreateDirectory(Path.GetDirectoryName(outpath));
-				using (var ds = cpk.GetChildByIndex(i).AsFile.DataStream)
+				using (var ds = GetFile(cpk, i, decompress))
 				using (FileStream fs = new FileStream(outpath, FileMode.Create, FileAccess.Write)) {
 					StreamUtils.CopyStream(ds, fs, ds.Length);
 				}
 			}
+		}
+
+		public static void PrintDecompressUsage() {
+			Console.WriteLine("Usage: compressed.bin decompressed.bin");
+		}
+
+		public static int Decompress(List<string> args) {
+			string inpath = null;
+			string outpath = null;
+
+			try {
+				for (int i = 0; i < args.Count; ++i) {
+					switch (args[i]) {
+						default:
+							if (inpath == null) { inpath = args[i]; } else if (outpath == null) { outpath = args[i]; } else { PrintDecompressUsage(); return -1; }
+							break;
+					}
+				}
+			} catch (IndexOutOfRangeException) {
+				PrintDecompressUsage();
+				return -1;
+			}
+
+			if (inpath == null) {
+				PrintDecompressUsage();
+				return -1;
+			}
+
+			if (outpath == null) {
+				outpath = inpath + ".dec";
+			}
+
+			using (var infile = new HyoutaUtils.Streams.DuplicatableFileStream(inpath))
+			using (var outfile = new FileStream(outpath, FileMode.Create)) {
+				utf_tab_sharp.CpkUncompress.uncompress(infile, 0, infile.Length, outfile);
+			}
+
+			return 0;
+		}
+
+		public static void PrintCompressUsage() {
+			Console.WriteLine("Usage: decompressed.bin compressed.bin");
+		}
+
+		public static int Compress(List<string> args) {
+			string inpath = null;
+			string outpath = null;
+
+			try {
+				for (int i = 0; i < args.Count; ++i) {
+					switch (args[i]) {
+						default:
+							if (inpath == null) { inpath = args[i]; } else if (outpath == null) { outpath = args[i]; } else { PrintCompressUsage(); return -1; }
+							break;
+					}
+				}
+			} catch (IndexOutOfRangeException) {
+				PrintCompressUsage();
+				return -1;
+			}
+
+			if (inpath == null) {
+				PrintCompressUsage();
+				return -1;
+			}
+
+			if (outpath == null) {
+				outpath = inpath + ".cmp";
+			}
+
+			using (var infile = new HyoutaUtils.Streams.DuplicatableFileStream(inpath))
+			using (var outfile = new FileStream(outpath, FileMode.Create)) {
+				utf_tab_sharp.CpkCompress.compress(infile, 0, infile.Length, outfile);
+			}
+
+			return 0;
 		}
 	}
 }
