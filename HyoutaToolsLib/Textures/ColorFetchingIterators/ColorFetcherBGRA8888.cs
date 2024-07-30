2e8dc213e232b5c7b99fdcff71fed42978241b8c
Admiral H. Curtiss <pikachu025@gmail.com>
2024-07-30 19:25:48 +0200

    Add some Texture utility stuff

---
+++ HyoutaToolsLib/Textures/ColorFetchingIterators/ColorFetcherBGRA8888.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HyoutaUtils;
+
+namespace HyoutaTools.Textures.ColorFetchingIterators {
+	public class ColorFetcherBGRA8888 : IColorFetchingIterator {
+		private Stream SourceStream;
+		private long PixelCount;
+
+		public ColorFetcherBGRA8888( Stream stream, long width, long height ) {
+			SourceStream = stream;
+			PixelCount = width * height;
+		}
+
+		public IEnumerator<Color> GetEnumerator() {
+			for ( long i = 0; i < PixelCount; ++i ) {
+				yield return ColorFromARGB8888( SourceStream.ReadUInt32() );
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() {
+			return GetEnumerator();
+		}
+
+		public static Color ColorFromARGB8888( uint color ) {
+			int b = (int)( ( color & 0x000000FF )       );
+			int g = (int)( ( color & 0x0000FF00 ) >>  8 );
+			int r = (int)( ( color & 0x00FF0000 ) >> 16 );
+			int a = (int)( ( color & 0xFF000000 ) >> 24 );
+			return Color.FromArgb( a, r, g, b );
+		}
+	}
+}
--- HyoutaToolsLib/Textures/ColorFetchingIterators/ColorFetcherIndexed4Bits.cs
+++ HyoutaToolsLib/Textures/ColorFetchingIterators/ColorFetcherIndexed4Bits.cs
@@ -12,19 +12,26 @@ namespace HyoutaTools.Textures.ColorFetchingIterators {
 	public class ColorFetcherIndexed4Bits : IColorFetchingIterator {
 		private Stream SourceStream;
 		private long PixelCount;
+		private bool Flipped;
 		private Color[] colors;
 
-		public ColorFetcherIndexed4Bits( Stream stream, long width, long height, IColorFetchingIterator colorConverter ) {
+		public ColorFetcherIndexed4Bits( Stream stream, long width, long height, IColorFetchingIterator colorConverter, bool flipped = false ) {
 			SourceStream = stream;
 			PixelCount = width * height;
+			Flipped = flipped;
 			colors = colorConverter.ToArray();
 		}
 
 		public IEnumerator<Color> GetEnumerator() {
 			for ( long i = 0; i < PixelCount / 2; ++i ) {
 				byte b = SourceStream.ReadUInt8();
-				yield return colors[( b >> 4 ) & 0x0F];
-				yield return colors[b & 0x0F];
+				if (Flipped) {
+					yield return colors[b & 0x0F];
+					yield return colors[(b >> 4) & 0x0F];
+				} else {
+					yield return colors[(b >> 4) & 0x0F];
+					yield return colors[b & 0x0F];
+				}
 			}
 		}
 
